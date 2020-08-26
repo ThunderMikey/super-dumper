5174cefbdb461b513aa4fca2ebf0c09d3a146589
MerelyLogical <qieerbushe7@gmail.com>
2020-08-26 21:10:12 +0100

    restructure tesco code

--- src/main.py
+++ src/main.py
@@ -4,27 +4,23 @@ from bs4 import BeautifulSoup as bs
 from functools import reduce
 import operator
 import stores.asda as asda
-#import stores.tesco
+import stores.tesco as tesco
 #import stores.sainsburys
 import stores.waitrose as waitrose
 import stores.ocado as ocado
 
 store_funcs = {
-        "asda"       : (asda.filter_items             , asda.get_name_price_quantity),
-#        "tesco"      : (tesco.filter_items           , tesco.get_name_price_quantity),
-#        "sainsburys" : (sainsburys.filter_items , sainsburys.get_name_price_quantity),
+        "asda"       : (asda.filter_items         , asda.get_name_price_quantity),
+        "tesco"      : (tesco.filter_items        , tesco.get_name_price_quantity),
+#        "sainsburys" : (sainsburys.filter_items   , sainsburys.get_name_price_quantity),
         "waitrose"   : (waitrose.filter_items     , waitrose.get_name_price_quantity),
-        "ocado"      : (ocado.filter_items,         ocado.get_name_price_quantity)
+        "ocado"      : (ocado.filter_items        , ocado.get_name_price_quantity)
         }
 
 parser = argparse.ArgumentParser(description='HTML list to CSV processor')
-parser.add_argument('--store', '-s', type=str, required=True,
-        choices=store_funcs.keys(),
-        help='choose a store name')
-parser.add_argument('--input', '-i', type=str, required=True,
-        help='HTML input file')
-parser.add_argument('--output', '-o', type=str, required=True,
-        help='CSV output file')
+parser.add_argument('--store', '-s', type=str, required=True, choices=store_funcs.keys(), help='choose a store name')
+parser.add_argument('--input', '-i', type=str, required=True, help='HTML input file') 
+parser.add_argument('--output', '-o', type=str, required=True, help='CSV output file')
 args = parser.parse_args()
 
 htmlFileName = args.input
--- src/stores/tesco.py
+++ src/stores/tesco.py
@@ -3,25 +3,6 @@ import csv, re, argparse
 from bs4 import BeautifulSoup as bs
 from shared_py.funcs import get_the_only_element
 
-goe = get_the_only_element
-
-
-parser = argparse.ArgumentParser(description='Tesco order, HTML list to CSV processor')
-parser.add_argument('--input', '-i', type=str, required=True,
-        help='HTML input file. The top level should be <ul>')
-parser.add_argument('--output', '-o', type=str, required=True,
-        help='CSV output file')
-args = parser.parse_args()
-
-htmlFileName = args.input
-csvFileName = args.output
-
-htmlFile = open(htmlFileName, 'r')
-htmlText = htmlFile.read()
-htmlFile.close()
-
-html = bs(htmlText, "html.parser")
-items = goe(html.find_all('ul', class_='product-list'))
 
 def get_the_only_element(l):
     if len(l) != 1:
@@ -63,9 +44,6 @@ def get_name_price_quantity(row):
     item_info = [name, str(total_price), quantity]
     return [info.strip() for info in item_info]
 
-data = [get_name_price_quantity(row) for row in items.find_all('li', recursive=False)]
-csvFile = open(csvFileName,'w')
-w = csv.writer(csvFile)
-w.writerows(data)
-
-csvFile.close()
+def filter_items(html):
+    items = get_the_only_element(html.find_all('ul', class_='product-list'))
+    return items.find_all('li', recursive=False)
