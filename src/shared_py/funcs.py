bf97748d0de146ff95742c52982a719fb2d27328
Mike Chen <mike@tmikey.tech>
2020-08-26 23:28:35 +0100

    fix Tesco

--- src/shared_py/funcs.py
+++ src/shared_py/funcs.py
@@ -1,7 +1,13 @@
+import sys
 # shared functions
 
 def get_the_only_element(l):
-    if len(l) != 1:
+    if len(l) > 1:
         raise RuntimeError("matched more than one element!\n", l)
+    elif len(l) == 0:
+        raise RuntimeError("matched none element!\n", l)
     else:
         return l[0]
+
+def eprint(msg):
+    print(msg, file=sys.stderr)
--- src/stores/tesco.py
+++ src/stores/tesco.py
@@ -2,41 +2,62 @@
 import csv, re, argparse
 from bs4 import BeautifulSoup as bs
 from shared_py.funcs import get_the_only_element
+from shared_py.funcs import eprint
+
+goe = get_the_only_element
 
 def get_name_price_quantity(row):
+    """
+        When Tesco items are out of stock, price and quantity disappears
+        * therefore, default to non-exit behaviour
+        * and print name to stderr for later fixing
+    """
+    priceOrQuantityNotFound = 0
     try:
-        name = row.find('h3').find('a').text
+        product = goe(row.find_all('div', class_='product-tile-wrapper', recursive=False))
+    except RuntimeError as err:
+        print("cannot find product")
+        print(err)
+        print(row)
+        exit(1)
+    try:
+        name = product.find('h3').find('a').text
     except AttributeError as err:
         print("can't find name")
         print(err)
-        print(row)
+        print(product)
         exit(1)
     try:
-        price_match = re.findall(
-                '\d+\.\d+',
-                row.find('div', class_='price-control-wrapper').find('span', class_='value').text
+        price = goe(
+                    re.findall(
+                    '\d+\.\d+',
+                    product.find('div', class_='price-control-wrapper').find('span', class_='value').text
+                    )
                 )
     except AttributeError as err:
-        print("can't find price")
+        print("can't find price, default to -1")
         print(err)
-        print(row)
-        exit(1)
+        print(product)
+        priceOrQuantityNotFound += 1
+        price = "-1"
     try:
-        quantity_match = re.findall(
-                '\d+',
-                row.find('input', class_='product-input')['value']
+        quantity = goe(
+                    re.findall(
+                    '\d+',
+                    product.find('input', class_='product-input')['value']
+                    )
                 )
-    except AttributeError as err:
-        print("can't find quantity")
+    except TypeError as err:
+        print("can't find quantity, default to 1")
         print(err)
-        print(row)
-        exit(1)
-    price = get_the_only_element(price_match)
-    quantity = get_the_only_element(quantity_match)
+        print(product)
+        priceOrQuantityNotFound += 1
+        quantity = "1"
+    if priceOrQuantityNotFound: eprint(name)
     total_price = float(price) * float(quantity)
     item_info = [name, str(total_price), quantity]
     return [info.strip() for info in item_info]
 
 def filter_items(html):
-    items = get_the_only_element(html.find_all('ul', class_='product-list'))
+    items = goe(html.find_all('ul', class_='product-list'))
     return items.find_all('li', recursive=False)
