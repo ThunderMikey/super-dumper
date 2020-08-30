1e7c76bc54cd03333a3149aafa8abc9c111a6eb7
Mike Chen <mike@tmikey.tech>
2020-08-30 17:21:05 +0100

    strip £ from price

--- src/stores/sainsburys.py
+++ src/stores/sainsburys.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-import csv, re, argparse
-from bs4 import BeautifulSoup as bs
-
 def filter_items(html):
         return html.find_all('div', class_='order-details__trolley-summary-item')
 
 def get_name_price_quantity(row):
         name = row.find_next('span').text
-        price = row.find_next('span').find_next('span').text
+        price = row.find_next('span').find_next('span').text.strip('£')
         quantity = row.div.span.text.split(': ')[1]
         return [name, price, quantity]
